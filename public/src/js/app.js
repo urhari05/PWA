555f5ee033ccc8c4edcff767ab2076ac547dc0e4
Harish Sunkerikar <h.sunkerikar@prowareness.nl>
2018-06-14 11:57:19 +0530

    Added a fetch event and install banner event

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,12 @@
+var deferredprompt;
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js").then(function() {
     console.log("Service Worker Registered");
   });
 }
+
+window.addEventListener("beforeinstallprompt", function(event) {
+  event.preventDefault();
+  deferredprompt = event;
+  return false;
+});
--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,15 +1,29 @@
-var shareImageButton = document.querySelector('#share-image-button');
-var createPostArea = document.querySelector('#create-post');
-var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var shareImageButton = document.querySelector("#share-image-button");
+var createPostArea = document.querySelector("#create-post");
+var closeCreatePostModalButton = document.querySelector(
+  "#close-create-post-modal-btn"
+);
 
 function openCreatePostModal() {
-  createPostArea.style.display = 'block';
+  createPostArea.style.display = "block";
+  if (deferredprompt) {
+    deferredprompt.prompt();
+    deferredprompt.userChioce.then(function(choiceResult) {
+      console.log(choiceResult.outcome);
+      if (choiceResult.outcome === "dismissed") {
+        console.log("User cancelled installation");
+      } else {
+        console.log("Use added to home screen");
+      }
+    });
+    deferredprompt = null;
+  }
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = 'none';
+  createPostArea.style.display = "none";
 }
 
-shareImageButton.addEventListener('click', openCreatePostModal);
+shareImageButton.addEventListener("click", openCreatePostModal);
 
-closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
